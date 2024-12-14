6bfe827a3f0f78f905e3f0e5be7ed0d303485865
VerbalThree <verbalthree00@gmail.com>
2024-12-13 21:46:31 -0300

    Adding a new font

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Arima } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
 export const lusitana = Lusitana({ subsets: ['latin'], weight:['400', '700']});
+export const arima = Arima({ subsets: ['latin'], weight:['400', '700']});
